a82a6cee4765e0a8e4ce788170099f356f892739
suemor <3100825062@qq.com>
2022-05-11 22:20:02 +0800

    update: readme.md

--- config.ts
+++ config.ts
@@ -21,12 +21,12 @@ export const opensource = {
 }
 export const motto = `<p align=center><strong>お可愛いこと。</strong></p>`
 export const github = {
-  name: 'Innei',
+  name: 'suemor233',
 }
 
 export const mxSpace = {
-  url: 'https://innei.ren',
-  api: 'https://api.innei.ren/v2',
+  url: 'https://suemor.com',
+  api: 'https://server.suemor.com/api/v2',
 }
 
 export const timeZone = 'Asia/Shanghai'
