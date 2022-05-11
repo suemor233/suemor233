1c1156e1c54229ba00383939a290b2ea72e77c7e
suemor <3100825062@qq.com>
2022-05-11 22:36:33 +0800

    update: readme.md

--- config.ts
+++ config.ts
@@ -1,23 +1,5 @@
 export const opensource = {
-  active: [
-    'mx-space/kami',
-    'mx-space/mx-server',
-    'mx-space/mx-admin',
-    'mx-space/api-client',
-  ],
-  toys: {
-    limit: 3,
-    random: true,
-    repos: [
-      'Innei/react-typewriter',
-      'Innei/simple-clock',
-      'Innei/shuffle-article',
-      'Innei/bump-version',
-      'Innei/candy',
-      'Innei/pageproxy',
-      'Innei/animate-uri',
-    ],
-  },
+
 }
 export const motto = `<p align=center><strong>お可愛いこと。</strong></p>`
 export const github = {
